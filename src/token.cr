b403aaaa638f92ceeac1054a67b4d680e38c55a0
Jamie Gaskins <jgaskins@hey.com>
2026-04-03 18:34:19 -0400

    Tweak some formatting
    I'm still trying to figure out how I want to organize all this. The hard
    part of it is that some of these tokens can be used in multiple clauses,
    so you can't really organize them by which part of the query it goes in.
    Some tokens go in several of them.

--- src/token.cr
+++ src/token.cr
@@ -10,29 +10,104 @@ module Parsegres
     Identifier
 
     # Keywords
-    Select; From; Where; And; Or; Not
-    Join; Inner; Left; Right; Full; Outer; Cross; Natural
-    On; Using; As
-    Group; By; Having
-    Order; Asc; Desc; Nulls; First; Last
-    Limit; Offset
-    In; Between; Like; ILike; Is; Null
-    Case; When; Then; Else; End
-    True; False
-    Distinct; All
+    Select
+    From
+    Where
+    And
+    Or
+    Not
+    Join
+    Inner
+    Left
+    Right
+    Full
+    Outer
+    Cross
+    Natural
+    On
+    Using
+    As
+    Group
+    By
+    Having
+    Order
+    Asc
+    Desc
+    Nulls
+    First
+    Last
+    Limit
+    Offset
+    In
+    Between
+    Like
+    ILike
+    Is
+    Null
+    Case
+    When
+    Then
+    Else
+    End
+    True
+    False
+    Distinct
+    All
     Exists
-    Union; Except; Intersect
-    With; Recursive
-    Insert; Into; Values; Default; Returning
-    Update; Set; Only
+    Union
+    Except
+    Intersect
+    With
+    Recursive
+    Insert
+    Into
+    Values
+    Default
+    Returning
+    Update
+    Set
+    Only
     Delete
-    Create; Table; Temp; Temporary; If
-    Primary; Key; References; Foreign; Check; Unique; Constraint
-    Alter; Add; Drop; Column; Rename; To; Cascade; Restrict
-    Index; Concurrently
-    View; Truncate; Sequence; Schema; Begin; Commit; Rollback; Savepoint; Release
-    Extension; Exclude; Type; Do
-    Over; Partition
+    Create
+    Table
+    Temp
+    Temporary
+    If
+    Primary
+    Key
+    References
+    Foreign
+    Check
+    Unique
+    Constraint
+    Alter
+    Add
+    Drop
+    Column
+    Rename
+    To
+    Cascade
+    Restrict
+    Index
+    Concurrently
+    View
+    Truncate
+    Sequence
+    Schema
+    Begin
+    Commit
+    Rollback
+    Savepoint
+    Release
+    Extension
+    Exclude
+    Type
+    Do
+    Over
+    Partition
+    For
+    Share
+    NoWait
 
     # Operators
     Eq           # =
@@ -78,5 +153,8 @@ module Parsegres
     EOF
   end
 
-  record Token, type : TokenType, value : ::String, pos : Int32
+  record Token,
+    type : TokenType,
+    value : ::String,
+    pos : Int32
 end
